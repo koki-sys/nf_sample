2a8642d25dc8379ffd941d59c1820349824e1c51
koki-sys <soraisu7@gmail.com>
2021-03-01 21:50:53 +0900

    feature: nf_sample developed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Row } from './Row'
+import { Banner } from './Banner'
+import { requests } from './request'
+import { Nav } from 'Nav'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Nav />
+            <Banner />
+            <Row
+                title="NETFLIX ORIGUINALS"
+                fetchUrl={requests.feachNetflixOriginals}
+                isLargeRow
+            />
+            <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+            <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+            <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+            <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+            <Row
+                title="Romance Movies"
+                fetchUrl={requests.feactRomanceMovies}
+            />
+            <Row
+                title="Documentaries"
+                fetchUrl={requests.feactDocumentMovies}
+            />
+        </div>
+    )
 }
-
-export default App;
+export default App
